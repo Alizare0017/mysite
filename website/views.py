514283d109c64e1fe4cc02b1798e73416736b288
Alizare0017 <alizare0017@gmail.com>
2022-03-10 12:42:56 +0330

    added general templates folder
    moved website templates to general folder of templates

--- website/views.py
+++ website/views.py
@@ -8,7 +8,7 @@ def home(request):
     return render(request,'website/home.html')
 
 def about(request):
-    return render(request,'about.html')
+    return render(request,'website/about.html')
 
 def contact(request):
-    return render(request,'contact.html')
+    return render(request,'website/contact.html')
