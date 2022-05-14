8b8a60e074d585823bb6eb7846cfad47055bd70a
Alizare0017 <alizare0017@gmail.com>
2022-05-14 17:59:08 +0430

    First model
    just created few field for contact table

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    # image
+    
+    # author
+    
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    # tag
+    # category
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    published_date = models.TimeField(null=True)
+    created_date = models.TimeField(auto_now_add=True)
+    updated_date = models.TimeField(auto_now=True)
