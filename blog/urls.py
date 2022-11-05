c51f3cba56a09dbf8b4740c09232f6e2de558f53
Alizare0017 <alizare0017@gmail.com>
2022-11-05 22:48:12 +0330

    Improve Data base Models
    Add author, image and category to models.py

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from turtle import home
 from unicodedata import name
 from django.urls import path
 from blog.views import *
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 app_name = 'blog'
 
@@ -10,4 +11,4 @@ urlpatterns = [
     path('blog/<int:pid>',blog_single,name='blog-single'),
     path('blog-home',blog_home,name='blog-home'),
     path('test/<str:testID>',test,name='test'),
-] 
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
